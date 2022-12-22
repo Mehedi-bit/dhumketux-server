29aeb4674cec18c197d78875aa6d4490879df1ef
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2022-12-22 22:51:39 +0600

    DhumketuX habitable exoplanets added

---
+++ src/models/planets.model.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
+
+const habitablePlanets = [];
+
+// FIND inhabitable planets
+function isHabitablePlanet(planet) {
+  // Check Confirmation, Amount of Sunlight and the Planetary radius
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+
+function loadPlanetsData () {
+    return new Promise((resolve, reject) => {
+
+        // Convert the csv data into object and store
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data','kepler_data.csv'))
+          .pipe(parse({
+            comment: '#',
+            columns: true,
+          }))
+          .on('data', (data) => {
+            // Check habitability criteria and move it to habitable planets
+            if (isHabitablePlanet(data)) {
+              habitablePlanets.push(data);
+            }
+          })
+          .on('error', (err) => {
+            console.log(err);
+            reject(err);
+          })
+          .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve();
+          });
+
+    });
+}
+
+
+
+  module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+  };
